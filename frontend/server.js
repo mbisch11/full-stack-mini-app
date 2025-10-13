cc9ea0a25b1395faae8e54fcae246531ad239dad
Brett Cunningham <brettc340@gmail.com>
2025-10-13 10:55:06 -0400

    yo 2

---
+++ frontend/server.js
@@ -0,0 +1,25 @@
+//console.log('Hello from Node')
+
+const http = require('http')
+
+const hostname = '127.0.0.1'
+
+const port = 3000
+
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/plain')
+    res.setHeader('Access-Control-Allow-Origin','*')
+
+    const {method, url} = req
+
+    res.write('Hello from Node.js!')
+
+    res.end()
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at ${hostname}:${port}.`)
+})
+
