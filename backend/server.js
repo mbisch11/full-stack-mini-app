ae9e19e56a1cdb150c1ed1b001571eef084167e3
mbisch11 <mbisch11@gmail.com>
2025-10-22 10:57:14 -0400

    Added login feature (need to fix infinite loading bug)

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,41 @@ app.get('/', (req, res) => {
 
 app.get('/signup/:user/:pass', (req, res) => {
     res.set('content-type', 'application/json');
+
+    const username = req.params.user;
+    const password = req.params.pass;
+    
     const filecontent = fs.readFileSync(filepath, {encoding: "utf-8"});
     console.log(JSON.parse(filecontent));
+});
+
+app.get('/login/:user/:pass', (req, res) => {
+    res.set('content-type', 'application/json');
+    var validUser = false;
+    const userData = null;
+
+    const username = req.params.user;
+    const password = req.params.pass;
+
+    const filecontent = fs.readFileSync(filepath, {encoding: "utf-8"});
+    const parseData = JSON.parse(filecontent);
+
+    parseData.array.forEach(person => {
+        if(person.user == username && person.pass == password){
+            validUser = true;
+            userData = person;
+        }
+    });
+
+    if(validUser){
+        res.send(userData);
+    }else{
+        res.send({
+            'user':null,
+            'pass':null,
+            'id':-1
+        })
+    }
 });
 
 app.listen(port, () => {
