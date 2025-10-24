be2a9d38c044cc6c4026ac3d9ad7f5371c34b0d7
Brett Cunningham <brettc340@gmail.com>
2025-10-24 10:40:48 -0400

    Add io.js and ignore macOS files

---
+++ frontend/io.js
@@ -0,0 +1,33 @@
+//CODE FROM CLASS
+
+// const fs = require("fs")
+// // fs.writeFileSync("text.txt","Hello there!")
+// // let test = fs.readFileSync("test.txt")
+// // console.log(`Test info: ${test}`)
+
+// let students = []
+// const student1 = {
+//     name: 'Jane Doe',
+//     gpa: 3.9,
+//     id: 12345
+// }
+
+// fs.writeFileSync("students.json", JSON.stringify(students))
+
+// let read_students_str = fs.readFileSync("students.json")
+
+// let read_students = JSON.parse(read_students_str)
+
+// const student2 = {
+//     name: "Jim Smith",
+//     gpa: 3.6,
+//     id: 4321
+// }
+
+// read_students.push(student2)
+
+// students.push(student1)
+// console.log(students)
+
+// //Use it for saving info on the server without using a DB
+
